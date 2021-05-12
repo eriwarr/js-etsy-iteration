aa1f17fb7bebedcc89f0a829264ceaae79d176d7
Eric Warren II <ericwarrenii@Erics-MacBook-Pro.local>
2021-05-12 08:52:54 -0400

    1st update

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,21 @@ const totalPrice = pricesArray.reduce(function(a, b) {
 
 const avg = Math.round(totalPrice / items.length * 100) / 100
 
-console.log(`The average price is ${avg}`)
+console.log(`The average price is $${avg}`)
 
 //2. Show me how to get an array of items that cost between $14.00 and
 //$18.00 USD. Please `console.log` the answer.
+
+for(const property in items){
+  items.forEach(function (item){
+    console.log(property)
+  })
+
+}
+
+
+
+
+// 3. Show me how to find the item with a "GBP" currency code
+//  and print its name and price. Please `console.log` the
+//   one you find.
